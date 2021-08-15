6e46b2cc63cf4b4e7669d5ff8d7ce129fc94df4b
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2021-08-15 10:31:51 +0300

    Create quiz15.py

---
+++ quiz15.py
@@ -0,0 +1,43 @@
+import random
+
+class QA:
+  def __init__(self, question, correctAnswer, otherAnswers):
+    self.question = question
+    self.corrAnsw = correctAnswer
+    self.otherAnsw = otherAnswers
+
+qaList = [QA("Which is Venus surface temperature?", "464 °C", ["100 °C", "354 °C"]),
+QA("Which is Mercury's mean diameter?", "4880 km", ["2153 km", "6789 km", "1558 km"]),
+QA("Which is Jupiter's's eccentricity?", "0,0489", ["1", " 0,022", "0,5"]),
+QA("Which is Ganymede's eccentricity?", "0.0013", ["1", "0,25"]),
+QA("Which is Mars eccentricity?", "0,0934", ["0,0235", "0,0182", "0,003", "0,001"])]
+
+corrCount = 0
+random.shuffle(qaList)
+for qaItem in qaList:
+  print(qaItem.question)
+  print("Possible answers are:")
+  possible = qaItem.otherAnsw + [qaItem.corrAnsw] # square brackets turn correct answer into list for concatenating with other list
+  random.shuffle(possible)
+  count = 0 # list indexes start at 0 in python
+  while count < len(possible):
+    print(str(count+1) + ": " + possible[count])
+    count += 1
+  print("Please enter the number of your answer:")
+  userAnsw = input()
+  while not userAnsw.isdigit():
+    print("That was not a number. Please enter the number of your answer:")
+    userAnsw = input()
+  userAnsw = int(userAnsw)
+  while not (userAnsw > 0 and userAnsw <= len(possible)):
+    print("That number doesn't correspond to any answer. Please enter the number of your answer:")
+    userAnsw = input()
+  if possible[userAnsw-1] == qaItem.corrAnsw:
+    print("Your answer was correct.")
+    corrCount += 1
+  else:
+    print("Your answer was wrong.")
+    print("Correct answer was: " + qaItem.corrAnsw)
+  print("")
+
+print("You answered " + str(corrCount) + " of " + str(len(qaList)) + " questions correctly.")
